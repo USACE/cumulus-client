7554bf15063467f5c37f0016988b67813458a39c
Brett Palmberg <29417036+brettpalmberg@users.noreply.github.com>
2020-06-17 08:47:27 -0500

    updated cumulus api url (#2)

--- cumulus_api/config.py
+++ cumulus_api/config.py
@@ -1,4 +1,4 @@
 # Configuration variables for the Cumulus API
 
 # Root URL to the Cumulus API (DEVELOPMENT API)
-CUMULUS_API = 'https://cumulus.rsgis.dev/development'
+CUMULUS_API = 'https://api.rsgis.dev/development/cumulus'
